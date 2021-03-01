9aa7d9d937e6dc4696a7cf589bbe3480c82ee9d4
LeightonManningQA <lmanning@QA.com>
2021-03-01 10:56:44 +0000

    First commit with structure of program for frontend and backend

---
+++ Frontend/qacinemas/src/App.js
@@ -0,0 +1,13 @@
+
+import component from './Components/component';
+
+function App() {
+  return (
+    <div className="App">
+     <p>Hello world!</p>
+     <component/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/qacinemas/src/Components/component.jsx
@@ -0,0 +1,6 @@
+const component = () => {
+    return (
+        <h1>Welcome</h1>
+    )
+}
+export default component
