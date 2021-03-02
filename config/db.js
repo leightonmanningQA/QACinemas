e4762b58b1082e1bba765b5b433a811da9039b43
Will McCudden <wmccudden-hughes@qa.com>
2021-03-02 09:31:46 +0000

    backend for film complete

--- config/db.js
+++ config/db.js
@@ -2,17 +2,27 @@
 const mongoose = require(`mongoose`);
 
 const {Schema, model} = mongoose;
-const {DB_URL, DB_NAME} = require('../config/consts.json');
+const {DB_URL} = require('../config/consts.json');
 
-// const taskSchema = new Schema({
-//     name: {type:String, required:true },  //, min length etc
-//     description: String,
-//     completed: Boolean
-// });
+const discussionSchema = new Schema({
+    film: {type:String, required:true },  //, min length etc
+    rating: String,
+    comment: String
+});
 
-// const Task = model('Task',taskSchema);
+const filmSchema = new Schema({
+    title: {type:String, required:true },  //, min length etc
+    description: {type:String, required:true },
+    picture: {type: String, required:true},
+    releaseYear: {type: String, required: true},
+    newRelease:{type: Boolean, requried:true}
+});
 
-mongoose.connect(`mongodb://${DB_URL}/${DB_NAME}`, {useNewUrlParser:true, useUnifiedTopology:true}, (err) =>{
+const Discussion = model('Discussion',discussionSchema);
+
+const Film = model('Film', filmSchema)
+
+mongoose.connect(`${DB_URL}`, {useNewUrlParser:true, useUnifiedTopology:true}, (err) =>{
     if(err){
         console.error(err);
     }else{
@@ -20,4 +30,5 @@ mongoose.connect(`mongodb://${DB_URL}/${DB_NAME}`, {useNewUrlParser:true, useUni
     }
 });
 
-// module.exports= {"Task":Task};
+module.exports= {"Discussion":Discussion};
+module.exports = {"Film":Film};
