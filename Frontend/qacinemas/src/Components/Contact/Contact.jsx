2e175458c09f5b4889af7029585608fc66117336
RQAJohnson <RQAJohnson@qa.com>
2021-03-02 14:23:31 +0000

    changed the format of the contact information

--- Frontend/qacinemas/src/Components/Contact/Contact.jsx
+++ Frontend/qacinemas/src/Components/Contact/Contact.jsx
@@ -1,13 +1,61 @@
 import React from 'react';
 import { Table } from 'reactstrap';
 import { Container, Row, Col } from 'reactstrap';
+import { Toast, ToastBody, ToastHeader } from 'reactstrap';
 
 const Contact = (props) => {
     return (
     <>
-    <h1>Contact Us</h1>   
+    <h1>Contact Us</h1>  
+    <div>
+      <div className="p-3 my-2 rounded">
+        <Toast>
+          <ToastHeader>
+            Phone Number
+          </ToastHeader>
+          <ToastBody>
+            02086295348
+          </ToastBody>
+        </Toast>
+      </div> 
+      <div className="p-3 my-2 rounded">
+        <Toast>
+          <ToastHeader>
+            Email Address
+          </ToastHeader>
+          <ToastBody>
+          <form action="" method="">
+    <div class="col-md">
+        <label class="form-check-label">First Name:</label>
+        <input type="text" name="username" id="username" class="form-control" required/>
+    </div>
+    <div class="col-md">
+        <label class="form-check-label">Email:</label>
+        <input type="email" name="email" id="email" class="form-control" required/>
+    </div>
+    <div class="col-md">
+        <label class="form-check-label">Subject:</label>
+        <input type="text" name="subject" id="subject" class="form-control" required/>
+    </div>
+        <button outline color="primary" type="submit"><a href = "mailto: RJohnson@qa.com">submit</a></button>
+    </form>
+          </ToastBody>
+        </Toast>
+      </div> 
+      <div className="p-3 my-2 rounded">
+        <Toast>
+          <ToastHeader>
+            Site Address
+          </ToastHeader>
+          <ToastBody>
+            25 Langbury Lane, T12 M58
+          </ToastBody>
+        </Toast>
+      </div> 
+      </div>
 
-    <Container>
+
+    {/* <Container>
     <Table>
         <thead>
             <tr>
@@ -29,12 +77,11 @@ const Contact = (props) => {
         <button outline color="primary" type="submit"><a href = "mailto: RJohnson@qa.com">submit</a></button>
     </form>
     </th>
-
-    <th>Site Address: </th>
+    <th>Site Address: 25 Langbury Lane, T12 M58</th> 
             </tr>
         </thead>
     </Table>
-    </Container> */
+    </Container> */}
     
 
 
@@ -44,8 +91,6 @@ const Contact = (props) => {
             <tr>
           <th>Phone Number: </th>
           <th><a href = "mailto: RJohnson@qa.com">Email Address</a></th> */}
-        
-   
     </>
     )
 }
