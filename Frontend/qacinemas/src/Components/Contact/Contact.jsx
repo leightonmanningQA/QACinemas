246a5cb9ce3327ed77e3ae918b33aebe739bae6d
RQAJohnson <RQAJohnson@qa.com>
2021-03-02 15:16:55 +0000

    commented code deleted

--- Frontend/qacinemas/src/Components/Contact/Contact.jsx
+++ Frontend/qacinemas/src/Components/Contact/Contact.jsx
@@ -6,92 +6,50 @@ import { Toast, ToastBody, ToastHeader } from 'reactstrap';
 const Contact = (props) => {
     return (
     <>
-    <h1 className="text-white" textAlign="centre">Contact Us</h1>  
+    <h1 className="text-white" textAlign="centre">Contact Us</h1>
     <div>
-      <div className="p-3 my-2 rounded">
-        <Toast>
-          <ToastHeader>
-            Phone Number
-          </ToastHeader>
-          <ToastBody>
-            02086295348
-          </ToastBody>
-        </Toast>
+        <div className="p-3 my-2 rounded">
+            <Toast>
+                <ToastHeader>
+                    Phone Number
+                </ToastHeader>
+                <ToastBody>
+                    02086295348
+                </ToastBody>
+            </Toast>
       </div> 
       <div className="p-3 my-2 rounded">
-        <Toast>
-          <ToastHeader>
-            Email Address
-          </ToastHeader>
-          <ToastBody>
-          <form action="" method="">
-    {/* <div class="col-md">
-        <label class="form-check-label">First Name:</label>
-        <input type="text" name="username" id="username" class="form-control" required/>
-    </div> */}
-    <div class="col-md">
-        <label class="form-check-label">Email:</label>
-        <input type="email" name="email" id="email" class="form-control" required/>
-    </div>
-    <div class="col-md">
-        <label class="form-check-label">Subject:</label>
-        <input type="text" name="subject" id="subject" class="form-control" required/>
-    </div>
-        <button outline color="primary" type="submit">
-            <a href = "mailto: savannah.vaithilingam@qa.com?subject=Issue with website&body=To Monster Cinema,">submit</a></button>
-    </form>
-          </ToastBody>
-        </Toast>
+            <Toast>
+                <ToastHeader>
+                    Email Address
+                </ToastHeader>
+                <ToastBody>
+                    <form action="" method="">
+                        <div class="col-md">
+                            <label class="form-check-label">Email:</label>
+                            <input type="email" name="email" id="email" class="form-control" required/>
+                        </div>
+                        <div className="col-md">
+                            <label className="form-check-label">Subject:</label>
+                            <input type="text" name="subject" id="subject" class="form-control" required/>
+                        </div>
+                        <button outline color="primary" type="submit">
+                            <a href = "mailto: savannah.vaithilingam@qa.com?subject=Issue with website&body=To Monster Cinema,">submit</a></button>
+                    </form>
+                </ToastBody>
+            </Toast>
       </div> 
       <div className="p-3 my-2 rounded">
-        <Toast>
-          <ToastHeader>
-            Site Address
-          </ToastHeader>
-          <ToastBody>
-            25 Langbury Lane, T12 M58
-          </ToastBody>
-        </Toast>
+            <Toast>
+                <ToastHeader>
+                    Site Address
+                </ToastHeader>
+                <ToastBody>
+                    25 Langbury Lane, T12 M58
+                </ToastBody>
+             </Toast>
       </div> 
       </div>
-
-
-    {/* <Container>
-    <Table>
-        <thead>
-            <tr>
-          <th>Phone Number: </th>
-            <th>
-    <form action="" method="">
-    <div class="col-md">
-        <label class="form-check-label">First Name:</label>
-        <input type="text" name="username" id="username" class="form-control" required/>
-    </div>
-    <div class="col-md">
-        <label class="form-check-label">Email:</label>
-        <input type="email" name="email" id="email" class="form-control" required/>
-    </div>
-    <div class="col-md">
-        <label class="form-check-label">Subject:</label>
-        <input type="text" name="subject" id="subject" class="form-control" required/>
-    </div>
-        <button outline color="primary" type="submit"><a href = "mailto: RJohnson@qa.com">submit</a></button>
-    </form>
-    </th>
-    <th>Site Address: 25 Langbury Lane, T12 M58</th> 
-            </tr>
-        </thead>
-    </Table>
-    </Container> */}
-    
-
-
-    {/* <Container>
-    <Table>
-        <thead>
-            <tr>
-          <th>Phone Number: </th>
-          <th><a href = "mailto: RJohnson@qa.com">Email Address</a></th> */}
     </>
     )
 }
