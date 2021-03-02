dee0b4959a7593da7be364f004e214476fcd6a84
RQAJohnson <RQAJohnson@qa.com>
2021-03-02 15:26:49 +0000

    added white styling to the h1

--- Frontend/qacinemas/src/Components/About/About.jsx
+++ Frontend/qacinemas/src/Components/About/About.jsx
@@ -7,7 +7,7 @@ import { Toast, ToastBody, ToastHeader } from 'reactstrap';
 const About = (props) => {
     return (
         <>
-        <h1>Meet The Team</h1>
+        <h1 className="text-white">Meet The Team</h1>
     <Row xs="1" sm="2" md="4">
     <div className="p-3 my-2 rounded">
         <Toast>
@@ -49,58 +49,12 @@ const About = (props) => {
       </Row>
 
       <div>
-        <p>Follow the link below to our contact page</p>
+        <p className="text-white">Follow the link below to our contact page</p>
         <ul>
             <li><Link to="/Contact">Contact Us</Link></li>
         </ul>
     </div>
-</>
-        /* <Col>
-        <Card>
-        <CardHeader>Team Monster</CardHeader>
-            <CardBody>
-                <CardTitle tag="h5">Emmanuel Lomax</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <a class="nav-link" href="https://github.com/qamanny" target="_blank">Manny</a>
-            </CardBody>
-            <CardFooter>Footer</CardFooter>
-        </Card> 
-        </Col>
-        <Col>
-        <Card>
-            <CardHeader>Team Monster</CardHeader>
-            <CardBody>
-                <CardTitle tag="h5">Leighton Manning</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <a class="nav-link" href="https://github.com/leightonmanningQA" target="_blank">Leighton</a>
-            </CardBody>
-            <CardFooter>Footer</CardFooter>
-         </Card> 
-        </Col>
-        <Col>
-        <Card>
-            <CardHeader>Team Monster</CardHeader>
-            <CardBody>
-                <CardTitle tag="h5">Rianna Johnson</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <a class="nav-link" href="https://github.com/RQAJohnson" target="_blank">Rianna</a>
-            </CardBody>
-            <CardFooter>Footer</CardFooter>
-        </Card> 
-        </Col>
-        <Col>
-        <Card>
-            <CardHeader>Team Monster</CardHeader>
-            <CardBody>
-                <CardTitle tag="h5">William McCudden-Hughes</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <a class="nav-link" href="https://github.com/willmccuddenQA" target="_blank">Will</a>
-            </CardBody>
-            <CardFooter>Footer</CardFooter>
-        </Card> 
-        </Col> */
-     
-   
+</>  
 
     )
 }
