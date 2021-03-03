8301d7c5b7d5c6be11e2f30a180bcbbbb27e775e
Will McCudden <wmccudden-hughes@qa.com>
2021-03-03 17:08:46 +0000

    create booking functionality finished for front end

---
+++ Frontend/qacinemas/src/Components/TicketBookings/PayLink.jsx
@@ -0,0 +1,8 @@
+
+const PayLink = () => {
+    return (
+        <h4 className="text-white">Pay For Tickets</h4>
+    );
+}
+
+export default PayLink
--- Frontend/qacinemas/src/Components/TicketBookings/TicketBookings.jsx
+++ Frontend/qacinemas/src/Components/TicketBookings/TicketBookings.jsx
@@ -1,6 +1,35 @@
+import BookLink from "./BookLink";
+import PayLink from "./PayLink";
+import BookForm from "./BookForm";
+import { Container, Row, Col } from 'reactstrap';
+import { useState } from 'react';
+
 const TicketBookings = () => {
+
+    const [showForm, setShowForm] = useState(false);
+
     return (
-        <h1>Welcome to ticket bookings</h1>
-    )
+        <Container>
+            <br />
+            <Row>
+                <Col sm="12" md={{ size: 4, offset: 4 }}>
+                    <h1 className="text-white">Ticket Bookings</h1>
+                </Col>
+            </Row>
+            <br />
+            <Row>
+                <Col sm={{ size: 'auto', offset: 3 }}>
+                    <BookLink setShowForm = {setShowForm}/>
+                </Col>
+                <Col sm={{ size: 'auto', offset: 2 }}>
+                    <PayLink/>
+                </Col>
+            </Row>
+            <br/>
+            <br/>
+            <br/>
+            <BookForm showForm = {showForm}/>
+        </Container>
+    );
 }
 export default TicketBookings
