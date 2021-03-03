25eb6d110e166b77cbcf451e1a83a5a1ccc34e3c
RQAJohnson <RQAJohnson@qa.com>
2021-03-03 09:49:21 +0000

    added index and animating values

--- Frontend/qacinemas/src/Components/Screens/Screens.jsx
+++ Frontend/qacinemas/src/Components/Screens/Screens.jsx
@@ -26,6 +26,26 @@ const items = [
   ];
 
 const Screens = (props) => { 
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
+  
+    const next = () => {
+      if (animating) return;
+      const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+      setActiveIndex(nextIndex);
+    }
+  
+    const previous = () => {
+      if (animating) return;
+      const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+      setActiveIndex(nextIndex);
+    }
+  
+    const goToIndex = (newIndex) => {
+      if (animating) return;
+      setActiveIndex(newIndex);
+    }
+    
     return (
         <h1>Decor for Deluxe and Standard seats</h1>
     )
