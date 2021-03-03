ce2a5eeec4562584b3b2e90cc7db8bed49c30e06
RQAJohnson <RQAJohnson@qa.com>
2021-03-03 09:30:40 +0000

    Adding carousel from reactstrap

--- Frontend/qacinemas/src/Components/Screens/Screens.jsx
+++ Frontend/qacinemas/src/Components/Screens/Screens.jsx
@@ -1,4 +1,31 @@
-const Screens = () => { 
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
+const items = [
+    {
+      id: 1,
+      altText: 'Slide 1',
+      caption: 'Slide 1'
+    },
+    {
+      id: 2,
+      altText: 'Slide 2',
+      caption: 'Slide 2'
+    },
+    {
+      id: 3,
+      altText: 'Slide 3',
+      caption: 'Slide 3'
+    }
+  ];
+
+const Screens = (props) => { 
     return (
         <h1>Decor for Deluxe and Standard seats</h1>
     )
