557b5bc2bbd5bd567438f78e2a53827e0ea33b7d
Emmanuel <qamanny@academytrainee.com>
2021-03-03 12:04:01 +0000

    Finished getterThere, so I guess I'm getting there

--- Frontend/qacinemas/src/Components/PlacesToGo/PlacesToGo.jsx
+++ Frontend/qacinemas/src/Components/PlacesToGo/PlacesToGo.jsx
@@ -32,26 +32,9 @@ const PlacesToGo = () => {
                 <p>Website: http://londonfilmmuseum.com/</p>
                 <p>Description: Film based exhibits of impactful British cinema, 
                 make sure to check out the 'Bond In Motion' exhibit to see your favourite Bond cars in the flesh.</p>
-                <p>Deals: Keep your ticket from the exhibit and get 20% of your ticket for 'No Time to Die'.</p>
+                <p>Deals: Keep your ticket from the exhibit and get 20% off your ticket for 'No Time to Die'.</p>
             </div>
         </div>
   );
 };
-
-
-
-
-
-
-
-    // return (
-
-    //     <div className="container">
-    //         <h1> Places to Go</h1>
-
-    //         <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=17-18%20Henrietta%20St%20London+(Monster%20Cinema)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-
-    //     </div>
-
-    // )
 export default PlacesToGo
