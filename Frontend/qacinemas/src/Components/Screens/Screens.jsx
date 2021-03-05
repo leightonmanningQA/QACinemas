eaf726417093e34081f7793084423875ebd66305
RQAJohnson <RQAJohnson@qa.com>
2021-03-05 12:36:24 +0000

    installed animated slides to change the screens component

--- Frontend/qacinemas/src/Components/Screens/Screens.jsx
+++ Frontend/qacinemas/src/Components/Screens/Screens.jsx
@@ -7,33 +7,42 @@ import {
   CarouselCaption
 } from 'reactstrap';
 
+import Slider from 'react-animated-slider';
+import horizontalCss from 'react-animated-slider/build/horizontal.css';
+
+import "./Screens.css"
+
 const items = [
     {
       id: 1,
       src:'http://photos.cinematreasures.org/production/photos/191157/1478638675/large.JPG?1478638675',
       altText: 'Slide 1',
-      caption: 'Slide 1'
+      caption: 'Standard: Seating Area',
+      description: 'Up to 200 seats within the standard seating area.'
     },
     {
       id: 2,
-      src:'https://lh3.googleusercontent.com/proxy/AfOt_P2E4ZDjFCt1b93575twLjnQ9Vl3z4Sj_-FJAl4-E7niotLi5mSReVevFr9Wq3oDJ5wnRkXGIT9Yv_nu8vi0MqHm6t4U7h0Q9AwUPmFYUE4teR4WuA53u96WCxNc2AW5tpZk1w',
+      src:'https://i.pinimg.com/originals/eb/37/99/eb3799a5bfac17ccf55372a66c020969.jpg',
       altText: 'Slide 2',
-      caption: 'Slide 2'
+      caption: 'Standard: Concession/Lobby Area',
+      description: 'Concession Stand and lobby area. Home to amazing snacks and a few arcade games'
     },
     {
       id: 3,
       src:'https://www.bonvivant.co.uk/journal/wp-content/uploads//2012/12/electric_shoreditch_interior1.jpg',
       altText: 'Slide 3',
-      caption: 'Slide 3'
+      caption: 'Deluxe: Seating Area',
+      description: 'Members only area. With no more than 50 seats wiithin our deluxe seating area.'
     },
     {
         id: 4,
         src:'https://storage.googleapis.com/rhinos/uploads/2019/07/6e88ddd1-directors-suite-1-e1562831043556-960x540.jpg',
         altText: 'Slide 4',
-        caption: 'Slide 4'
+        caption: 'Deluxe: Box Suites',
+        description: 'Enjoy fine dining and cocktails before or after a movie with friends/family or on your own.'
 
       }
-  ];
+  ]; 
 
 const Screens = (props) => { 
     const [activeIndex, setActiveIndex] = useState(0);
@@ -72,9 +81,25 @@ const Screens = (props) => {
   });
 
   return (
-      <>      
-    <h1 className="text-white" align="center">Standard and Deluxe Seating</h1>
-    <div>
+      <>
+      <h1 className="text-white" align="center">Standard and Deluxe Seating</h1>
+      <br></br>
+      <Slider classNames={horizontalCss} autoplay={2000}>
+        {items.map((item, index) => (
+          <div
+            key={index}
+            style={{ background: `url('${item.src}') no-repeat center center` }}
+          >
+            <div className="slides text-white inner" align="center">
+              <h4>{item.caption}</h4>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
+    </Slider>
+
+    
+    {/* <div>
       <style>
         {
           `.custom-tag {
@@ -94,7 +119,7 @@ const Screens = (props) => {
         <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
         <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
       </Carousel>
-    </div>
+    </div> */}
     </>
   );
 }
