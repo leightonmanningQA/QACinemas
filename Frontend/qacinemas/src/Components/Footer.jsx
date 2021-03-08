64bce9e9d0fc53bcd286ed448d46bd1a50615388
Will McCudden <wmccudden-hughes@qa.com>
2021-03-08 16:34:48 +0000

    small changes to footer, not finished yet

--- Frontend/qacinemas/src/Components/Footer.jsx
+++ Frontend/qacinemas/src/Components/Footer.jsx
@@ -14,9 +14,8 @@ const Footer = () => {
                                 <div class="col-xl-2 col-md-4 col-sm-4 col-12">
                                     <h6 class="mb-3 mb-lg-4 bold-text "><b>MENU</b></h6>
                                     <ul class="list-unstyled">
-                                        <li>Home</li>
-                                        <li>About</li>
-                                        <li>Contact Us</li>
+                                        <li href="/about"><a href="/about">About</a></li>
+                                        <li><a href="/contact">Contact Us</a></li>
                                     </ul>
                                 </div>
                                 <div class="col-xl-2 col-md-4 col-sm-4 col-12">
@@ -28,12 +27,6 @@ const Footer = () => {
                             <div class="row ">
                                 <div class="col-xl-8 col-md-4 col-sm-4 col-auto my-md-0 mt-5 order-sm-1 order-3 align-self-end">
                                     <p class="social text-muted mb-0 pb-0 bold-text"> <span class="mx-2"><i class="fa fa-facebook" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-linkedin-square" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-twitter" aria-hidden="true"></i></span> <span class="mx-2"><i class="fa fa-instagram" aria-hidden="true"></i></span> </p><small class="rights"><span>&#174;</span> Qa Cinemas All Rights Reserved</small>
-                                </div>
-                                <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-1 align-self-end ">
-                                    <h6 class="mt-55 mt-2 text-muted bold-text"><b>ANIRUDH SINGLA</b></h6><small> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> anirudh@gmail.com</small>
-                                </div>
-                                <div class="col-xl-2 col-md-4 col-sm-4 col-auto order-2 align-self-end mt-3 ">
-                                    <h6 class="text-muted bold-text"><b>RISHABH SHEKHAR</b></h6><small><span><i class="fa fa-envelope" aria-hidden="true"></i></span> rishab@gmail.com</small>
                                 </div>
                             </div>
                         </div>
