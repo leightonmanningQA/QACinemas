248ee98961e6a57e91dcab7052e8c4bdcfe46e52
Emmanuel <qamanny@academytrainee.com>
2021-03-10 09:28:16 +0000

    Improved looks

--- Frontend/qacinemas/src/Components/GettingThere/GettingThere.jsx
+++ Frontend/qacinemas/src/Components/GettingThere/GettingThere.jsx
@@ -3,24 +3,25 @@ import cinema4 from "../images/cinema4-medium.png"
 const GettingThere = () => {
     return (
         <div align="justify" className="container">
-            <h1 align="justify" className="text-white">Cinema Details</h1>
-            <justify>
-                <img align="center" width="600" height="600" src= {cinema4} />
-            </justify>
+            <h1 align="center" className="text-white">Cinema Details</h1>
+            <br></br>
+            <center>
+                <img width="600" height="600" src= {cinema4}/>
+            </center>
             <br></br>
             <br></br>
-            <h4 className="text-white">17-18 Henrietta St</h4>
-            <h4 className="text-white">Covent Garden</h4>
-            <h4 className="text-white">London</h4>
+            <h4 align="center" className="text-white">17-18 Henrietta St</h4>
+            <h4 align="center" className="text-white">Covent Garden</h4>
+            <h4 align="center" className="text-white">London</h4>
             <br></br>
             <hr></hr>
             <h4 className="text-white"><u>Telephone</u></h4>
             <br></br>
             <h5 className="text-white"> To speak to a representative on our ticket booking line please call us on 0873 435 9061 (calls cost 13ppm plus your telephone company's access charge).</h5>
             <br></br>
-            <h4 className="text-white"><u>Public Transport</u></h4>
-            <br></br>
             <hr></hr>
+            <h4 className="text-white" ><u>Public Transport</u></h4>
+            <br></br>
             <h5 className="text-white">BY TUBE</h5>
             <h5 className="text-white"> Covent Garden is on the Piccadilly Line. Covent Garden's tube station is a very short walk from the Market Building at the end of James Street. Covent Garden underground station only has lifts up from the platforms so it can be slow-going at busy times. During peak times we suggest getting off at Leicester Square (Piccadilly and Northern Line),
                  which is a short walk to Covent Garden.</h5>
@@ -40,10 +41,10 @@ const GettingThere = () => {
             <br></br>
             <hr></hr>
             <h1 className="text-white"> Find us at</h1>
+            <br></br>
             <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=17-18%20Henrietta%20St%20London+(Monster%20Cinema)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
             </div>
-            )
             </div>
-         )
+    )
 }
 export default GettingThere
--- Frontend/qacinemas/src/Components/PlacesToGo/PlacesToGo.jsx
+++ Frontend/qacinemas/src/Components/PlacesToGo/PlacesToGo.jsx
@@ -6,12 +6,15 @@ const PlacesToGo = () => {
         <br></br>
         <h1 align="center" className="text-white">Nearby Venues</h1>
         <br></br>
+        <br></br>
             <div align="center" className="text-white">
-                <img src="https://lh5.googleusercontent.com/p/AF1QipP0_55ERMDFSI5-LDqOfd2YYcDJuHAIDYdE5lbu=w408-h544-k-no" alt="Cafe"/>
                 <h3> WA Cafe Japanese patisserie LTD</h3>
-                <p>Phone: (+44) 2072405567</p>
+                <img src="https://lh5.googleusercontent.com/p/AF1QipP0_55ERMDFSI5-LDqOfd2YYcDJuHAIDYdE5lbu=w408-h544-k-no" alt="Cafe"/>
+                <br></br>
+                <br></br>
+                <p>Phone: <a className="text-primary" href="tel:020-724-05567">(+44) 02072405567</a></p>
                 <p>Address: 5 New Row, Covent Garden, London WC2N 4LH</p>
-                <p>Website: http://www.wacafe.co.uk/</p>
+                <p>Website: <a href="https://www.wacafe.co.uk/" target="_blank" rel="noopener">https://www.wacafe.co.uk/</a></p>
                 <p>Description: WA is a small, independent patisserie located in Ealing Broadway and Covent Garden.</p>
                 <p>Deals: Bring your cinema ticket for a 10% discount.</p>
             </div>
@@ -19,11 +22,14 @@ const PlacesToGo = () => {
             <br></br>
             <hr></hr>
             <div align="center" className="text-white">
-                <img src="https://lh5.googleusercontent.com/p/AF1QipPUGJ7WUBSh2UQKZstLrU8ul-rDVa7n4HWkgkNl=w408-h306-k-no" alt="The National Gallery"/>
                 <h3> The National Gallery</h3>
-                <p>Phone: (+44) 2077472885</p>
+                <br></br>
+                <img src="https://lh5.googleusercontent.com/p/AF1QipPUGJ7WUBSh2UQKZstLrU8ul-rDVa7n4HWkgkNl=w408-h306-k-no" alt="The National Gallery"/>
+                <br></br>
+                <br></br>
+                <p>Phone: <a className="text-primary" href="tel:020-774-72885">(+44) 2077472885</a></p>
                 <p>Address: Trafalgar Square, Charing Cross, London WC2N 5DN</p>
-                <p>Website: https://www.nationalgallery.org.uk/</p>
+                <p>Website: <a href="https://www.nationalgallery.org.uk" target="_blank" rel="noopener">https://www.nationalgallery.org.uk</a></p>
                 <p>Description: Home of the nation's art</p>
                 <p>Deals: None at present</p>
             </div>
@@ -31,11 +37,14 @@ const PlacesToGo = () => {
             <br></br>
             <hr></hr>
             <div align="center" className="text-white">
-                <img src="https://lh5.googleusercontent.com/p/AF1QipP3526Azuy3P-_N7eiGpBa8yRTEKzFmbEVUHcxR=w408-h306-k-no" alt="London Film Museum"/>
                 <h3> London Film Museum </h3>
-                <p>Phone: (020) 78364913</p>
+                <br></br>
+                <img src="https://lh5.googleusercontent.com/p/AF1QipP3526Azuy3P-_N7eiGpBa8yRTEKzFmbEVUHcxR=w408-h306-k-no" alt="London Film Museum"/>
+                <br></br>
+                <br></br>
+                <p>Phone:<a className="text-primary" href="tel:020-783-64913"> (+44) 2078364913</a></p>
                 <p>Address: 45 Wellington St, Covent Garden, London WC2E 7BN</p>
-                <p>Website: http://londonfilmmuseum.com/</p>
+                <p>Website: <a href="http://londonfilmmuseum.com" target="_blank" rel="noopener">http://londonfilmmuseum.com</a></p>
                 <p>Description: Film based exhibits of impactful British cinema, 
                 make sure to check out the 'Bond In Motion' exhibit to see your favourite Bond cars in the flesh.</p>
                 <p>Deals: Keep your ticket from the exhibit and get 20% off your ticket for 'No Time to Die'.</p>
